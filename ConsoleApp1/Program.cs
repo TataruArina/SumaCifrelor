5ac997356d9046e6d670e3cc27e4368c74809124
TataruArina <CIHCahul No.23@DESKTOP-VILIMEH>
2021-03-11 18:48:28 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Introdu numarul");
+            int nr = int.Parse(Console.ReadLine());
+            while (nr > 0)
+            {
+                int mod = (nr % 10);
+                sum = (sum + mod);
+                nr = (nr / 10);
+
+            }
+            Console.WriteLine("Suma cifrelor numarului este: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
